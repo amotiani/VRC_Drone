fd3f1de270c6c9afa2678d570a449137483d16da
amotiani <87703657+amotiani@users.noreply.github.com>
2025-11-06 00:51:45 -0800

    Refactor drone movement controls and gravity handling
    Refactor drone control logic for VR and desktop inputs, updating variable names and removing unused code. Adjust gravity handling and streamline control flow.

--- Assets/scripts/droneMove.cs
+++ Assets/scripts/droneMove.cs
@@ -10,32 +10,26 @@ public class droneMove : UdonSharpBehaviour
     public Rigidbody rigid;     //Rigidbody of the drone (assigned in inspector)
     private BoxCollider seatCollider;   //Seat collider of the drone seat
 
-    //Very weird naming: roll speed used for both pitch AND roll, moveSpeed used for thrust, yawspeed used for yaw & fwdSpeed used for forward movt.
-    [SerializeField] public float rollSpeed, moveSpeed, yawSpeed, fwdSpeed;
-    public float ogrollSpeed, ogMoveSpeed, ogYawSpeed, ogFwdSpeed;  //Original speeds (start values of these speeds).
+    //vrRoll speed used for both vrPitch AND vrRoll, thrustSpeed used for thrust, vrYawSpeed used for vrYaw
+    [SerializeField] public float rollSpeed, thrustSpeed, vrYawSpeed, dragValue;
     public GameObject _seat;    //Drone Seat gameobject
-    public bool seated=false;      //Boolean check if player is seated
-    private Quaternion rotation;    //rotation variable is of a Quaternion type which stores rotations in Unity. A quaternion is a four-tuple of real numbers {x,y,z,w}
-    private Vector3 position;
-    public Slider m_slide, d_slide, ad_slide, t_slide, y_slide, r_slide, p_slide, n_slide, g_slide;
+    public bool seated = false;      //Boolean check if player is seated
+    public UnityEngine.Vector3 gScale = new Vector3(0,-9.81f,0);
+    private UnityEngine.Quaternion rotation;    //rotation variable is of a Quaternion type which stores rotations in Unity. A quaternion is a four-tuple of real numbers {x,y,z,w}
+    private UnityEngine.Vector3 position;
+    public Slider m_slide, ad_slide, t_slide, y_slide, r_slide, p_slide, n_slide;
     public VRC.SDK3.Components.VRCStation seat;
-    private Vector3 gScale, directionVectorFwd, directionVectorBwd, directionVectorRight, directionVectorLeft;
-    public Vector3 directionVector;
+    private UnityEngine.Vector3 directionVectorFwd, directionVectorBwd, directionVectorRight, directionVectorLeft;
+    public UnityEngine.Vector3 directionVector;
     public bool resultantDirection = true;
-    private float pitch, throttle, yaw, roll, nitro, mass, drag;
+    private float vrPitch, vrThrottle, vrYaw, vrRoll, input_horizontal_axis, input_vertical_axis, desktopPitch, desktopRoll;
+    private bool resetPosition = false;
 
     public void Start()
     {
-        gScale = Physics.gravity;
         seat.disableStationExit = true;
-        mass = rigid.mass;
-        drag = rigid.drag;
         rotation = transform.rotation;
         position = transform.position;
-        directionVectorFwd = (transform.up + transform.forward).normalized;
-        directionVectorBwd = (transform.up - transform.forward).normalized;
-        directionVectorRight = (transform.up + transform.right).normalized;
-        directionVectorLeft = (transform.up - transform.right).normalized;
         seatCollider = _seat.GetComponent<BoxCollider>();
     }
 
@@ -44,136 +38,71 @@ public class droneMove : UdonSharpBehaviour
         rigid.MoveRotation(rotation);
     }
 
-    //Function to convert euler angles from 0,360 scale to -180,180 scale. This allows for checking -ve degree rotations.
-    private float NormalizeAngle(float angle){
-        if(angle>180f){
-            angle-=360f;
-        }
-        return angle;
-    }
-    void HandleRotations(){
-
-        float angleX = NormalizeAngle(transform.rotation.eulerAngles.x);
-        float angleY = NormalizeAngle(transform.rotation.eulerAngles.y);
-        float angleZ = NormalizeAngle(transform.rotation.eulerAngles.z);
-
-        if (angleZ< 7f && angleZ > -7f && angleX < 7f && angleX > -7f)
-        {
-            //Debug.Log("up");
-            resultantDirection=false;
-        }
-        else if (angleX > 7f && angleX < 88f)
-        {
-            //Debug.Log("fwd");
-            directionVector = directionVectorFwd;
-            resultantDirection = true;
-        }
-        else if (angleX < 7f && angleX > 88f)
-        {
-            //Debug.Log("bwd");
-            directionVector = directionVectorBwd;
-            resultantDirection = true;
-        }
-        else if (angleZ < 7f && angleZ > 88f)
-        {
-            //Debug.Log("right");
-            directionVector = directionVectorRight;
-            resultantDirection=false;
-        }
-        else if (angleZ > 7f && angleZ < 88f)
-        {
-            //Debug.Log("left");
-            directionVector = directionVectorLeft;
-            resultantDirection=false;
-        }
-    }
-
     void DesktopControls(){
 
-            if (Input.GetKey(KeyCode.A))
+            if (input_horizontal_axis != 0)
             {
-                rigid.AddRelativeTorque(-Vector3.up * (yawSpeed / 2) * Time.fixedDeltaTime, ForceMode.Impulse);
+                rigid.AddRelativeTorque(input_horizontal_axis * Vector3.up * (vrYawSpeed / 2), ForceMode.Force);
             }
-            if (Input.GetKey(KeyCode.D))
+            if (input_vertical_axis != 0)
             {
-                rigid.AddRelativeTorque(Vector3.up * (yawSpeed / 2) * Time.fixedDeltaTime, ForceMode.Impulse);
+                rigid.AddRelativeForce(Vector3.up * thrustSpeed * input_vertical_axis, ForceMode.Force);
             }
-            if (Input.GetKey(KeyCode.W))
+            if (desktopPitch != 0)
             {
-                if (!resultantDirection)
-                {
-                    rigid.AddRelativeForce(Vector3.up * moveSpeed, ForceMode.Impulse);
-                }
-                else
-                {
-                    rigid.AddRelativeForce(directionVector * (moveSpeed / 2), ForceMode.Impulse);
-                }
+                rigid.AddRelativeTorque(desktopPitch * Vector3.right * (rollSpeed / 2), ForceMode.Force);
             }
-            if (Input.GetKey(KeyCode.UpArrow))
+            if (desktopRoll != 0)
             {
-                rigid.AddRelativeTorque(Vector3.right * (rollSpeed / 2) * Time.fixedDeltaTime, ForceMode.Impulse);
-            }
-            if (Input.GetKey(KeyCode.DownArrow))
-            {
-                rigid.AddRelativeTorque(-Vector3.right * (rollSpeed / 2) * Time.fixedDeltaTime, ForceMode.Impulse);
-            }
-            if (Input.GetKey(KeyCode.LeftArrow))
-            {
-                rigid.AddRelativeTorque(Vector3.forward * (rollSpeed / 2) * Time.fixedDeltaTime, ForceMode.Impulse);
-            }
-            if (Input.GetKey(KeyCode.RightArrow))
-            {
-                rigid.AddRelativeTorque(-Vector3.forward * (rollSpeed / 2) * Time.fixedDeltaTime, ForceMode.Impulse);
+                rigid.AddRelativeTorque(desktopRoll * Vector3.forward * (rollSpeed / 2), ForceMode.Force);
             }
     }
 
     void VRControls(){
 
-            if (Input.GetButton("Oculus_CrossPlatform_SecondaryThumbstick") || Input.GetKeyDown(KeyCode.T))
+            if (vrYaw != 0)
             {
-                rigid.AddRelativeForce(Vector3.forward * moveSpeed * 5f, ForceMode.Impulse);
+                rigid.AddRelativeTorque(Vector3.up * (vrYawSpeed / 2) * vrYaw, ForceMode.Force);
             }
 
-            if (yaw != 0)
+            if (vrThrottle >= 0)
             {
-                rigid.AddRelativeTorque(Vector3.up * (yawSpeed / 2) * yaw * Time.fixedDeltaTime, ForceMode.Impulse);
+                rigid.AddRelativeForce(Vector3.up * vrThrottle * thrustSpeed, ForceMode.Force);
             }
 
-            if (throttle > 0)
+            if (vrRoll != 0)
             {
-                if (!resultantDirection)
-                {
-                    rigid.AddRelativeForce(Vector3.up * throttle * moveSpeed, ForceMode.Impulse);
-                }
-                else
-                {
-                    rigid.AddRelativeForce(directionVector * throttle * moveSpeed, ForceMode.Impulse);
-                }
+                rigid.AddRelativeTorque(-Vector3.forward * (rollSpeed / 2) * vrRoll, ForceMode.Force);
             }
 
-            if (roll != 0)
+            if (vrPitch != 0)
             {
-                rigid.AddRelativeForce(-Vector3.forward * roll * Time.fixedDeltaTime * 10f, ForceMode.Impulse);
-                rigid.AddRelativeTorque(-Vector3.forward * (rollSpeed / 2) * roll * Time.fixedDeltaTime, ForceMode.Impulse);
-                Debug.Log("Rolling amount:"+rollSpeed);
-            }
-
-            if (pitch != 0)
-            {
-                rigid.AddRelativeForce(Vector3.right * pitch * Time.fixedDeltaTime * 10f, ForceMode.Impulse);
-                rigid.AddRelativeTorque(Vector3.right * (rollSpeed / 2) * pitch * Time.fixedDeltaTime, ForceMode.Impulse);
+                rigid.AddRelativeTorque(Vector3.right * (rollSpeed / 2) * vrPitch, ForceMode.Force);
             }
     }
 
     private void Update()
     {
-        rigid.drag = d_slide.value;
+        vrPitch = Input.GetAxis("Oculus_CrossPlatform_SecondaryThumbstickVertical") * p_slide.value;
+        vrThrottle = Input.GetAxis("Oculus_CrossPlatform_PrimaryThumbstickVertical") * t_slide.value;
+        vrYaw = Input.GetAxis("Oculus_CrossPlatform_PrimaryThumbstickHorizontal") * y_slide.value;
+        vrRoll = Input.GetAxis("Oculus_CrossPlatform_SecondaryThumbstickHorizontal") * r_slide.value;
+
+        input_horizontal_axis = Input.GetAxis("Horizontal");
+        input_vertical_axis = Input.GetAxis("Vertical");
+        desktopPitch = 0.0f;
+        if(Input.GetKey(KeyCode.UpArrow)) desktopPitch = 1.0f;
+        if(Input.GetKey(KeyCode.DownArrow)) desktopPitch = -1.0f;
+        desktopRoll = 0.0f;
+        if(Input.GetKey(KeyCode.LeftArrow)) desktopRoll = 1.0f;
+        if(Input.GetKey(KeyCode.RightArrow)) desktopRoll = -1.0f;
+
         rigid.angularDrag = ad_slide.value;
         rigid.mass = m_slide.value;
+
         if(seated){
-            Physics.gravity = new Vector3(0,-g_slide.value,0);
             seatCollider.enabled=false;
-            HandleRotations();
+            // HandleRotations();
         }
         else{
             seatCollider.enabled=true;
@@ -187,17 +116,28 @@ public class droneMove : UdonSharpBehaviour
     }
     private void FixedUpdate()
     {
-        pitch = Input.GetAxis("Oculus_CrossPlatform_SecondaryThumbstickVertical") * p_slide.value;
-        throttle = Input.GetAxis("Oculus_CrossPlatform_PrimaryThumbstickVertical") * t_slide.value;
-        yaw = Input.GetAxis("Oculus_CrossPlatform_PrimaryThumbstickHorizontal") * y_slide.value;
-        roll = Input.GetAxis("Oculus_CrossPlatform_SecondaryThumbstickHorizontal") * r_slide.value;
-        nitro = fwdSpeed * n_slide.value;
+        
 
         if(seated){
-            //VR Controls
-            VRControls();    
-            /* DESKTOP CONTROLS */
-            DesktopControls();
+            rigid.AddForce(Vector3.down * gScale.magnitude, ForceMode.Acceleration);
+
+            bool isThrusting = (vrThrottle > 0) || (input_vertical_axis != 0);
+
+            if(!isThrusting && rigid.velocity.y > 0)    //no input & still moving up, apply drag
+            {
+                UnityEngine.Debug.Log("Apply drag");
+                Vector3 downwardDrag = Vector3.down * dragValue * rigid.velocity.y;
+                rigid.AddForce(downwardDrag, ForceMode.Acceleration);
+            }
+
+            if (vrPitch != 0 || vrThrottle != 0 || vrYaw != 0 || vrRoll != 0)
+            {
+                VRControls();
+            }
+            else
+            {
+                DesktopControls();
+            }
         }
     }
 
